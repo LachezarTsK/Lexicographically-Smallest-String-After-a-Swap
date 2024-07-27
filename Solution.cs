1f9fe276009f5bde13acf3bff5f6e5e4c747f92c
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-27 15:13:31 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,29 @@
+
+using System;
+
+public class Solution
+{
+    public string GetSmallestString(string input)
+    {
+        char[] inputAsArray = input.ToCharArray();
+
+        for (int i = 0; i < inputAsArray.Length - 1; ++i)
+        {
+            int x = inputAsArray[i] - '0';
+            int y = inputAsArray[i + 1] - '0';
+            if (x % 2 == y % 2 && x > y)
+            {
+                swap(inputAsArray, i, i + 1);
+                break;
+            }
+        }
+        return string.Join("", inputAsArray);
+    }
+
+    private void swap(char[] inputAsArray, int first, int second)
+    {
+        char temp = inputAsArray[first];
+        inputAsArray[first] = inputAsArray[second];
+        inputAsArray[second] = temp;
+    }
+}
